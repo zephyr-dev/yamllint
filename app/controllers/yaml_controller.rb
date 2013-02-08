5ce3553008f67398714dc7ad87a827c5af3572f6
prettycoder <maia@prettycoders.com>
2013-02-08 13:50:44 -0500

    use i18n to validate content.

--- app/controllers/yaml_controller.rb
+++ app/controllers/yaml_controller.rb
@@ -3,15 +3,21 @@ require 'ya2yaml'
 
   def parse
     @yaml = params[:yaml] || ""
+    file_name = Rails.root+'foo.yml'
+    file = File.new(file_name, "w+")
+    file.write(@yaml)
     if request.post?
       begin
-        @yaml = YAML.load(@yaml)
-        @yaml = @yaml.ya2yaml(:syck_compatible => true)
+        I18n::Backend::Simple.new.send(:load_translations, file.path)
+        # @yaml = YAML.load(@yaml)
+        # @yaml = @yaml.ya2yaml(:syck_compatible => true)
       rescue ArgumentError => ex
         @error = ex.message
         if @error =~ /line ([\d]+)/i
           @error_line = $1
         end
+      ensure
+        File.delete(file_name)
       end
     end
   end
