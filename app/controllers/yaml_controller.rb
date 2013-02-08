06cc8413d096295b3480e7bf5f119fb33f271103
prettycoder <maia@prettycoders.com>
2013-02-08 14:00:18 -0500

    capture all exception.

--- app/controllers/yaml_controller.rb
+++ app/controllers/yaml_controller.rb
@@ -11,7 +11,7 @@ require 'ya2yaml'
         I18n::Backend::Simple.new.send(:load_translations, file.path)
         # @yaml = YAML.load(@yaml)
         # @yaml = @yaml.ya2yaml(:syck_compatible => true)
-      rescue ArgumentError => ex
+      rescue Exception => ex
         @error = ex.message
         if @error =~ /line ([\d]+)/i
           @error_line = $1
